a30e2aef20fd015e76041a95400b44529621167f
EDILSON DE CASTRO SILVA BISPO <edilson.csbispo@senacsp.edu.br>
2023-06-03 10:35:46 -0300

    Nova atualização

---
+++ Aula 1/main.js
@@ -0,0 +1,26 @@
+// alert("Arquivo de outro local")
+
+// Tipos de variaveis 
+
+messagem = "Meu primeiro javascript";
+numeros_inteiro = 22 // int
+numeros_float = 23.5
+numeros_booler = true
+Array =  ["Edilson", "33", true ]
+
+nome = "Edilson",
+idade = 33,
+altura =  1.73,
+maior_idade = true,
+cidade = "Americana"
+numero_casa = 550
+
+meusDados = [nome, idade, altura, maior_idade, cidade, numero_casa]
+
+
+
+
+
+
+
+console.log("estou aprendendo");
