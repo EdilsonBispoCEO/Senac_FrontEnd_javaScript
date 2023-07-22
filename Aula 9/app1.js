2613a0ce9f30a01e7ba37b4e8b53b6c4c467114c
EDILSON DE CASTRO SILVA BISPO <edilson.csbispo@senacsp.edu.br>
2023-07-22 13:45:30 -0300

    Novas atualizões

--- Aula 9/app1.js
+++ Aula 9/app1.js
@@ -1,16 +1,21 @@
 const buttonElement =  document.getElementById('checkButton');
+const divElement =  document.getElementById('resultadoMensagem');
  buttonElement.addEventListener('click', function() {
-     alert('Você clicou no botão')
+   //  alert('Você clicou no botão')
 
      const numeroInput  = document.getElementById('numeroInput').value
      if (numeroInput < 10) { 
-        buttonElement.classList.remove('button-green') 
-        alert('O numero é Maior que 10') 
-        buttonElement.classList.add('button-red')     
+        divElement.classList.remove('button-green') 
+       // alert('O numero é Maior que 10') 
+       // buttonElement.classList.add('button-red')  
+        divElement.classList.add('button-red')   
+        divElement.innerHTML = 'O numero é Maior que 10';  
      } 
      else {
-        buttonElement.classList.remove('button-red')  
-        alert('O numero é Menor que 10')
-        buttonElement.classList.add('button-green')    
+        divElement.classList.remove('button-red')  
+       // alert('O numero é Menor que 10')
+       //  buttonElement.classList.add('button-green')  
+        divElement.classList.add('button-green')  
+        divElement.innerHTML = 'O numero é Menor que 10';  
      }
 });
